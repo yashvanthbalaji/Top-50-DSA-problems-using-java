4572ed86a544735d2da886f63b5052f8e0c08721
yashvanthbalaji <333yashvanthbalaji@gmail.com>
2026-04-25 21:19:19 +0530

    Renamed folders and fixed project structure

---
+++ ArrayProblems/MaximumSubArray.java
@@ -0,0 +1,33 @@
+
+public class MaximumSubArray {
+    public int subArray(int[] num)
+    {
+        int cur_sum = 0;
+        int max_sum = Integer.MIN_VALUE;
+        for (int i =0;i<num.length;i++)
+        {
+            int temp = cur_sum + num[i];
+            if(temp<num[i])
+            {
+                cur_sum = num[i];
+            }
+            else
+            {
+                cur_sum = temp;
+            }
+            //...........
+            if (cur_sum > max_sum)
+            {
+                max_sum = cur_sum;   
+            }
+            return max_sum;
+        }
+    }
+    public static void main(String[] args) {
+        int[] num = {-2,1,-3,4,-1,2,1,-5,4};
+        MaximumSubArray obj = new MaximumSubArray();
+        int result = obj.subArray(num);
+        System.out.println("Maximum sum = "+result);
+    }
+    
+}
